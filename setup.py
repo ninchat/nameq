2a8a341afbde70e8d2a69f55358ddef86f7a583e
Timo Savola <timo.savola@somia.fi>
2013-01-29 11:05:28 +0200

    0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
 	name             = "nameq",
-	version          = "0.4.1",
+	version          = "0.5",
 	maintainer       = "Timo Savola",
 	maintainer_email = "timo.savola@somia.fi",
 	url              = "https://github.com/somia/nameq",
