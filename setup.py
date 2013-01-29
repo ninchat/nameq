94293a8a6821c9df780674554f6c7503b6551bfe
Timo Savola <timo.savola@somia.fi>
2013-01-29 10:05:16 +0200

    debian: install as Python module

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name             = "nameq",
+	version          = "0.4.1",
+	maintainer       = "Timo Savola",
+	maintainer_email = "timo.savola@somia.fi",
+	url              = "https://github.com/somia/nameq",
+        license          = "MIT",
+	py_modules       = ["nameq"],
+)
