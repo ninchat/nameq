26e746fc2880689d6d1cf3dcc0f2678b5650dc01
Timo Savola <timo@ninchat.com>
2015-04-28 21:33:10 +0300

    python: programmatic nameq.dump interface

--- python/nameq/dump.py
+++ python/nameq/dump.py
@@ -2,10 +2,41 @@ from __future__ import absolute_import, print_function
 
 import argparse
 import json
+import logging
+import os
 import random
 import sys
 
 import boto
+
+log = logging.getLogger("nameq.dump")
+
+def dump_hosts(s3bucket, s3prefix, filter_features=None, single=False):
+	if s3prefix and not s3prefix.endswith("/"):
+		s3prefix += "/"
+
+	entries = []
+	error = None
+
+	for key in boto.connect_s3().get_bucket(s3bucket).list(s3prefix):
+		data = key.get_contents_as_string()
+		try:
+			entry_features = set(json.loads(data).get("features", ()))
+		except (TypeError, ValueError, KeyError) as e:
+			log.error("%s: %s", key.name, e)
+			error = e
+		else:
+			log.info("%s", key.name)
+			if not filter_features or filter_features & entry_features:
+				entries.append(key.name[len(s3prefix):])
+
+	if not entries and error:
+		raise error
+
+	if single:
+		entries = [random.choice(entries)]
+
+	return entries
 
 def main():
 	parser = argparse.ArgumentParser()
@@ -14,38 +45,21 @@ def main():
 	parser.add_argument("feature", nargs="*", help="feature names")
 	args = parser.parse_args()
 
-	filter_features = set(args.feature)
-
 	if "/" in args.s3location:
 		bucket, prefix = args.s3location.split("/", 1)
-		if prefix and not prefix.endswith("/"):
-			prefix += "/"
 	else:
-		bucket = args.s3location
-		prefix = ""
+		bucket, prefix = args.s3location, ""
 
-	entries = []
-	error = None
+	progname = os.path.basename(sys.argv[0])
 
-	for key in boto.connect_s3().get_bucket(bucket).list(prefix):
-		data = key.get_contents_as_string()
-		try:
-			entry_features = set(json.loads(data).get("features", ()))
-		except (TypeError, ValueError, KeyError) as e:
-			print("{}: {}".format(key.name, e), file=sys.stderr)
-			error = e
-		else:
-			if not filter_features or filter_features & entry_features:
-				entries.append(key.name[len(prefix):])
+	log_handler = logging.StreamHandler()
+	log_handler.setFormatter(logging.Formatter(progname + ": %(message)s"))
 
-	if entries:
-		if args.single:
-			print(random.choice(entries))
-		else:
-			for entry in entries:
-				print(entry)
-	elif error:
-		raise error
+	log.addHandler(log_handler)
+	log.setLevel(logging.INFO)
+
+	for entry in dump_hosts(bucket, prefix, set(args.feature), args.single):
+		print(entry)
 
 if __name__ == "__main__":
 	try:
